ff6c50f53ee0be572caa90651dcb948d4a332327
Ruslan-Trafimau <rus.trofimov@gmail.com>
2023-11-05 16:56:06 +0100

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,6 +4,8 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
+console.log(galleryItems);
+
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
